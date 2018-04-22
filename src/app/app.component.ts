07c9ce0cedfce0543c912258acbddc3334afa7d0
Alina <karabousik@gmail.com>
2018-04-22 21:31:40 +0300

    changed isVisible

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isVisible = false;
+  isVisible = true;
   title = 'My First Angular App!';
   myText = "Моя первая программа";
 
